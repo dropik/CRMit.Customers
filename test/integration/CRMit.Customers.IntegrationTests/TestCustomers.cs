ffe8cd90ae2f4aea3772a015b16d4d63e186197d
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-03-05 01:16:37 +0200

    Added IntegrationTests project

---
+++ test/integration/CRMit.Customers.IntegrationTests/TestCustomers.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace CRMit.Customers.IntegrationTests
+{
+    public class TestCustomers
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
